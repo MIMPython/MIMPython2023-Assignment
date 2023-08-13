0a498236034059c0726889b4f25909dac02f3e9a
QuanHoang <quanhoang.pm@gmail.com>
2023-08-14 01:59:57 +0700

    Module 01 id 05 assignments

---
+++ assignment_module01/module01_student05_NguyenQuocHieu/module01_assignment01_student05_NguyenQuocHieu.py
@@ -0,0 +1,7 @@
+"""
+a) Ngôn ngữ Python được tạo ra bởi Guido van Rossum và phát hành lần đầu năm 1991
+
+b) Những năm 1970 tại Anh, Đài BBC có cho phát sóng các tập phim của đoàn hài kịch siêu thực Monty Python. Monty Python nhanh chóng trở nên phổ biến và trở thành một trong những show truyền hình được yêu thích nhất trong các quốc gia nói tiếng Anh thời bấy giờ. Rossum hâm mộ chương trình này rất nhiều, và anh đã đặt tên cho “đứa con đầu lòng” của mình là Python, với hy vọng đứa con lớn lên có thể trở nên nổi tiếng và được người dân ưa chuộng.
+
+c) Tính tới ngày 12/08/2023, phiên bản Python mới nhất được phát hành là phiên bản 3.11.4
+"""
