75c5f5083dadc747be2d431b5c79fbdfaa9a4e54
Na Na <130862112+ThuyBachDuong@users.noreply.github.com>
2023-08-27 23:05:12 +0700

    Update module02_assignment04_student01_TranDacNhatAnh.py

--- assignment_module02/module02_student01_TranDacNhatAnh/module02_assignment04_student01_TranDacNhatAnh.py
+++ assignment_module02/module02_student01_TranDacNhatAnh/module02_assignment04_student01_TranDacNhatAnh.py
@@ -1,16 +1,14 @@
-#Đọc văn bản
-with open ('Week2BT4.txt', 'r', encoding='utf8') as file:
-    passage = file.readlines()
-    passage = ''.join(passage)
-#List các đối tượng cần kiểm tra
+# Đọc văn bản
+passage = 'Python was designed to be easy to understand and fun to use (its name came from Monty Python so a lot of its beginner tutorials reference it). Fun is a great motivator, and since you’ll be able to build prototypes and tools quickly with Python, many find coding in Python a satisfying experience. Thus, Python has gained popularity for being a beginner-friendly language, and it has replaced Java as the most popular introductory language at Top U.S. Universities.'
+# List các đối tượng cần kiểm tra
 wordList = ['Python', 'contains', 'experience', 'difficult']
-#Hàm kiểm tra xem 1 từ có xuất hiện trong đoạn
+# Hàm kiểm tra xem 1 từ có xuất hiện trong đoạn
 def wordCheck(word):
     if word in passage:
         return f"The word {word} is in the passage"
     else:
         return f"The word {word} is not in the passage"
-#Hàm đếm số lần xuất hiện của 1 từ trong đoạn
+# Hàm đếm số lần xuất hiện của 1 từ trong đoạn
 def wordCounter(passage, word):
     lst = str(passage).split(' ')
     count = 0
@@ -18,12 +16,12 @@ def wordCounter(passage, word):
         if word == w:
             count +=1
     return f"There are {count} '{word}' in the passage"
-#Hàm đếm số từ có trong đoạn
+# Hàm đếm số từ có trong đoạn
 def length(passage):
     lst = (passage).split()
     count = len(lst)
     return f"There are {count} words in the passage"
-#Hàm biến câu đầu tiên của đoạn văn thành in hoa
+# Hàm biến câu đầu tiên của đoạn văn thành in hoa
 def upperFirstSentence(passage):
     lst = (passage).split()
     for i in range(len(lst)):
@@ -32,7 +30,7 @@ def upperFirstSentence(passage):
             break
     passage = ' '.join(lst)
     return passage
-#Test
+# Test
 for word in wordList:
     print(wordCheck(word))
     
