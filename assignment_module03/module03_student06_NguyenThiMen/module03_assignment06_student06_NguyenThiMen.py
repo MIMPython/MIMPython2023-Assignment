0be9a8c1b982493d5f2ef0608ab05c7975d70b3c
thimenni <nguyenthimen_t67@hus.edu.vn>
2023-09-09 17:27:00 +0700

    Add module03_student06

---
+++ assignment_module03/module03_student06_NguyenThiMen/module03_assignment06_student06_NguyenThiMen.py
@@ -0,0 +1,33 @@
+# a).greatest common divisor with Euclid algorithm
+def gcd(a, b):
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
+
+# b). Tinh xac suat chon ngau nhien 2 so la 2 cap so nguyen to cung nhau tu tap hop
+# cac so nguyen duong khong qua n
+
+# Cach 2: Dem so cap so nguyen to cung nhau trong tat ca cap so co the chon
+n = int(input('Enter n: '))
+list = []
+for i in range(1, n+1):
+    tpl = ()
+    for j in range(1, n+1):
+        tpl = (i, j)
+        list.append(tpl)
+print(list)
+n_A2 = 0
+n_omega2 = len(list)
+for element in list:
+    a = element[0]
+    b = element[1]
+    if gcd(a, b) == 1:
+        n_A2 += 1
+print(n_A2)
+P = n_A2 / n_omega2
+print(P) # 0.63
+
+# c). P*(pi**b) = a
