20a88d8de731acd381be7bfe00d56a432fd0b389
NgocHieu0912 <tranngochieu_t67@hus.edu.vn>
2023-09-11 12:17:11 +0700

    Added module03_student04_TranNgocHieu.

---
+++ assignment_module03/module03_student04_TranNgocHieu.py/module03_assignment10_student04_TranNgocHieu.py
@@ -0,0 +1,17 @@
+# a)
+"""
+Vòng lặp "while" trong python và C++ có cách thức hoạt động tương tự
+nhau. Sự so sánh giữa "while" và "do...while" trong C++ đã được làm ở
+bài tập 9.
+"""
+
+# b)
+"""
+Có thể để chương trình trong vòng lặp chạy trước một lần, sau đó thực
+thi vòng lặp. Đây là cách hoạt động của "do...while".
+
+    statement()
+    while (condition):
+        statement()
+        
+"""
---
+++ assignment_module03/module03_student04_TranNgocHieu.py/module03_assignment13_student04_TranNgocHieu.py
@@ -0,0 +1,4 @@
+"""
+Ta có thể chứng minh được rằng tổng số chữ số của 2^n và 5^n 
+luôn là n + 1.
+"""
