25ed53f5c36a2c6b278adf0901d0b1c90b0e71a7
ThuyBachDuong <trandac1415@gmail.com>
2023-10-03 00:36:32 +0700

    khôi phục module04

--- assignment_module04/module04_student01_TranDacNhatAnh/module04_assignment08_student01_TranDacNhatAnh.py
+++ assignment_module04/module04_student01_TranDacNhatAnh/module04_assignment08_student01_TranDacNhatAnh.py
@@ -3,7 +3,7 @@ def factorial(n):
     for i in range(1, n+1):
         product *= i
     return product
-print(factorial(1558)) # 30414093201713378043612608166064768844377641568960512000000000000
+print(factorial(50)) # 30414093201713378043612608166064768844377641568960512000000000000
 # a) Đếm số chữ số ở tận cùng bên phải
 def mathCountZeroes(n): 
     # Ý tưởng toán học của hàm là, do số thừa số 2 trong tích n! luôn nhiều hơn so với số thừa số 5, vậy nên ta chỉ cần quan tâm đến
@@ -13,7 +13,7 @@ def mathCountZeroes(n):
         n //= 5
         zeroes += n
         return zeroes
-# print(mathCountZeroes(50)) # 10
+print(mathCountZeroes(50)) # 10
 # b) Chữ số khác 0 ở ngoài cùng bên phải
 def nonZeroRight(n):
     factor = 1
@@ -22,4 +22,4 @@ def nonZeroRight(n):
         while factor%10 == 0:
             factor //= 10
     return factor%10
-# print(nonZeroRight(50)) # 2
+print(nonZeroRight(50)) # 2
